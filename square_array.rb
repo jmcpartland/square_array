5e0c51645d8ddf3e75f707019fb3feb09e049fe4
joemcpartland@gmail.com <josephmcpartland@JM.hsd1.co.comcast.net>
2021-02-07 11:49:51 -0700

    commit tree b39ac21f5148b7d7c0911f5c6cad147228d4d22d

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+numbers = [1, 2, 3]
+
+def square_array(numbers)
+  square_numbers = []
+  numbers.each { |a| square_numbers << a ** 2 }
+  square_numbers
+    
 end
